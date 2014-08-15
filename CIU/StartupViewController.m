bf4d7662d969147f88316358f2b5a2b62b604a68
jhuang <jasonhuang@quickenloans.com>
2014-08-15 10:10:55 -0400

    side bar animation work

--- CIU/StartupViewController.m
+++ CIU/StartupViewController.m
@@ -15,7 +15,9 @@ NS_ENUM(NSUInteger, SideBarStatus){
 #define SIDE_BAR_CLOSE_DISTANCE 0.0f
 #define AVATAR_CELL_HEIGHT 102.0f
 #define OTHER_CELL_HEIGHT 44.0f
-@interface StartupViewController()<UITableViewDataSource,UITableViewDelegate>
+@interface StartupViewController()<UITableViewDataSource,UITableViewDelegate>{
+    CGPoint previousPoint;
+}
 @end
 
 @implementation StartupViewController
@@ -27,14 +29,48 @@ NS_ENUM(NSUInteger, SideBarStatus){
 
 -(void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (IBAction)handlePanContainerView:(id)sender {
+- (IBAction)handlePanContainerView:(UIPanGestureRecognizer *)sender {
+   CGPoint point = [sender translationInView:self.view];
+    float deltaX = point.x - previousPoint.x;
+    previousPoint = point;
+    if (self.containerViewLeadingSpaceConstraint.constant + deltaX <0 || self.containerViewLeadingSpaceConstraint.constant + deltaX >SIDE_BAR_OPEN_DISTANCE) {
+        return;
+    }
+    self.containerViewLeadingSpaceConstraint.constant += deltaX;
     
+    //reset
+    if (sender.state == UIGestureRecognizerStateCancelled || sender.state == UIGestureRecognizerStateEnded || sender.state == UIGestureRecognizerStateFailed) {
+        previousPoint = CGPointZero;
+        [self animateSideBarToOpenOrClose];
+    }
+}
+
+-(void)animateSideBarToOpenOrClose{
+    
+    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+    
+    if (self.containerViewLeadingSpaceConstraint.constant < SIDE_BAR_OPEN_DISTANCE/2) {
+        
+        self.containerViewLeadingSpaceConstraint.constant = SIDE_BAR_CLOSE_DISTANCE;
+    }else{
+        self.containerViewLeadingSpaceConstraint.constant = SIDE_BAR_OPEN_DISTANCE;
+    }
+    
+    [UIView animateWithDuration:.3 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+    }];
 }
 
 -(void)handleSideBarSlideOpen{
-    
+    [self animateSideBarWhenMenuTapped];
+}
+
+-(void)animateSideBarWhenMenuTapped{
     [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
     
     if (self.containerViewLeadingSpaceConstraint.constant==0) {
@@ -42,13 +78,12 @@ NS_ENUM(NSUInteger, SideBarStatus){
     }else{
         self.containerViewLeadingSpaceConstraint.constant = SIDE_BAR_CLOSE_DISTANCE;
     }
-
+    
     [UIView animateWithDuration:.3 animations:^{
         [self.view layoutIfNeeded];
     } completion:^(BOOL finished) {
         [[UIApplication sharedApplication] endIgnoringInteractionEvents];
     }];
-    
 }
 
 #pragma mark - UITableView
