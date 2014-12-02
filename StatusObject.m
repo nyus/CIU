a364ab8acc11253686323331e08b34a5e9523b6b
S H <sh88990@gmaiil.com>
2014-12-02 09:04:19 -0500

    fixed a crashed bug on surprise VC

---
+++ StatusObject.m
@@ -0,0 +1,32 @@
+//
+//  StatusObject.m
+//  CIU
+//
+//  Created by Sihang on 12/2/14.
+//  Copyright (c) 2014 Huang, Jason. All rights reserved.
+//
+
+#import "StatusObject.h"
+
+
+@implementation StatusObject
+
+@dynamic anonymous;
+@dynamic avatar;
+@dynamic commentCount;
+@dynamic createdAt;
+@dynamic isBadContent;
+@dynamic likeCount;
+@dynamic message;
+@dynamic objectId;
+@dynamic photoCount;
+@dynamic photoID;
+@dynamic picture;
+@dynamic posterFirstName;
+@dynamic posterLastName;
+@dynamic posterUsername;
+@dynamic statusCellHeight;
+@dynamic latitude;
+@dynamic longitude;
+
+@end
