8c35cd2192af8c2bad3d5c47d8e13685b37fea95
Sihang <sihang@krossover.com>
2015-06-24 16:37:56 -0400

    Change static string definitions into #define so that swift compiler would recognize them. Added profile image test on SignUpVC

--- DaDa Tests/DDAccessibilityLabelConstants.h
+++ DaDa Tests/DDAccessibilityLabelConstants.h
@@ -6,14 +6,21 @@
 //  Copyright (c) 2015 Huang, Sihang. All rights reserved.
 //
 
-static NSString *kUserNameTextFieldAccessibilityLabel = @"username";
-static NSString *kPassWordTextFieldAccessibilityLabel = @"password";
-static NSString *kRememberMeButtonAccessibilityLabel = @"rememberMe";
-static NSString *kForgotPasswordButtonAccessibilityLabel = @"forgotPassword";
-static NSString *kSignInButtonAccessibilityLabel = @"signIn";
-static NSString *kLogInWithFacebookButtonAccessibilityLabel = @"logInWithFacebook";
-static NSString *kSignUpButtonAccessibilityLabel = @"signUp";
-static NSString *kMenuButtonAccessibilityLabel = @"menu";
-static NSString *kLogOutButtonAccessibilityLabel = @"Log out";
-static NSString *kDeclineEULAButtonAccessibilityLabel = @"declineEULA";
-static NSString *kAcceptEULAButtonAccessibilityLabel = @"acceptEULA";
+#define kUserNameTextFieldAccessibilityLabel @"Username"
+#define kPassWordTextFieldAccessibilityLabel @"Password"
+#define kRememberMeButtonAccessibilityLabel @"Remember Me"
+#define kForgotPasswordButtonAccessibilityLabel @"Forgot Password"
+#define kSignInButtonAccessibilityLabel @"Sign In"
+#define kLogInWithFacebookButtonAccessibilityLabel @"Log In With Facebook"
+#define kSignUpButtonAccessibilityLabel @"Sign Up"
+#define kMenuButtonAccessibilityLabel @"Menu"
+#define kLogOutButtonAccessibilityLabel @"Log out"
+#define kDeclineEULAButtonAccessibilityLabel @"Decline EULA"
+#define kAcceptEULAButtonAccessibilityLabel @"Accept EULA"
+#define kProfileImageViewAccessibilityLabel @"Profile Picture"
+#define kEmailAccessibilityLabel @"Email"
+#define kFirstNameAccessibilityLabel @"First Name"
+#define kLastNameAccessibilityLabel @"Last Name"
+#define kPasswordAccessibilityLabel @"Password"
+
+//AccessibilityLabel
---
+++ DaDa Tests/TesterExtension.swift
@@ -0,0 +1,29 @@
+//
+//  TesterExtension.swift
+//  DaDa
+//
+//  Created by Sihang Huang on 6/24/15.
+//  Copyright (c) 2015 Huang, Sihang. All rights reserved.
+//
+
+import Foundation
+
+extension XCTestCase {
+    func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
+
+extension KIFTestActor {
+    func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
